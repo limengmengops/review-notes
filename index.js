a2e08c14f1808d6fd04cb6f73e0b72ce9809c16f
mm920906 <mm920906@sina.com>
2019-12-23 11:22:14 +0800

    update

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 var child = require('child_process');
  
 child.exec('deploy.sh', function(err, sto) {
-    console.log(11122);
     console.log(err);
     console.log(sto);//sto才是真正的输出，要不要打印到控制台，由你自己啊
 })
 
-console.log(1111);
